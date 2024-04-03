588cdecdaa0c8690033ef3bfab4f58c5b77d4944
Martin Wester <martin.driss.wester@gmail.com>
2024-04-03 11:39:09 +0200

    feat(Login): validate pseudo

--- prbd_2324_a01/ViewModel/LoginViewModel.cs
+++ prbd_2324_a01/ViewModel/LoginViewModel.cs
@@ -6,6 +6,28 @@ namespace prbd_2324_a01.ViewModel;
 
 public class LoginViewModel : ViewModelBase<User, PridContext>
 {
+    private string _pseudo;
+
+    public string Pseudo {
+        get => _pseudo;
+        set => SetProperty(ref _pseudo, value, () => Validate());
+    }
+
+    public override bool Validate() {
+        ClearErrors();
+
+        var member = Context.Users.Find(Pseudo);
+
+        if (string.IsNullOrEmpty(Pseudo))
+            AddError(nameof(Pseudo), "required");
+        else if (Pseudo.Length < 3)
+            AddError(nameof(Pseudo), "length must be >= 3");
+        else if (member == null)
+            AddError(nameof(Pseudo), "does not exist");
+
+        return !HasErrors;
+    }
+
     protected override void OnRefreshData() {
     }
 }

8e47dba529c4ba9d75af8a9f391bcdd361f36793
NICOLAS VANHORLE <vanhorlenicolas@gmail.com>
2024-05-15 12:29:55 +0200

    feat(tricountcards): suite de la creation des cards

--- prbd_2324_a01/ViewModel/TricountCardViewModel.cs
+++ prbd_2324_a01/ViewModel/TricountCardViewModel.cs
@@ -10,9 +10,21 @@ public class TricountCardViewModel : ViewModelBase<User, PridContext> {
         get => _tricount;
     }
 
+    public string BgColor => GetBalanceColor();
     public string Title => Tricount.Title;
+    public string Description => Tricount.Description;
+    public string Creator => Context.Users.Find(Tricount.Creator).FullName;
+    public string CreationDate => Tricount.CreatedAt.ToString();
+    public string LastOperation => "DATE";
+    public string Members => "MEMEBRS";
+
+
 
     public TricountCardViewModel(Tricount tricount) {
         _tricount = tricount;
+    }
+
+    private string GetBalanceColor() {
+        return "Red";
     }
 }

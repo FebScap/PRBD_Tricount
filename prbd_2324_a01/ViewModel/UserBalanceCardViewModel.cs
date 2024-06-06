93a3f7ae379799f26d3cb08d10b20e9ade529424
Nicolas Vanhorle <vanhorlenicolas@gmail.com>
2024-06-06 18:51:29 +0200

    feat(display tricount) : suite de la vue

--- prbd_2324_a01/ViewModel/UserBalanceCardViewModel.cs
+++ prbd_2324_a01/ViewModel/UserBalanceCardViewModel.cs
@@ -2,7 +2,9 @@
 using prbd_2324_a01.Model;
 using prbd_2324_a01.Utils;
 using PRBD_Framework;
+using SQLitePCL;
 using System.Text;
+using System.Windows.Navigation;
 
 namespace prbd_2324_a01.ViewModel;
 
@@ -18,16 +20,32 @@ public class UserBalanceCardViewModel : ViewModelBase<User, PridContext> {
 
 
     public string UserName => User.FullName;
-    public string Amount => string.Format("{0:0.00 €}", Math.Round(Tricount.GetMyExpenses(User.Id), 2));
-    public string NegativeVisibility => Tricount.GetMyExpenses(User.Id) < 0 ? "Visible" : "Collapse";
-    public string PositiveVisibility => Tricount.GetMyExpenses(User.Id) >= 0 ? "Visible" : "Collapse";
-    public string isSelfUserVisibility => User == CurrentUser ? "Visible" : "Collapse";
-    public string isNotSelfUserVisibility => User != CurrentUser ? "Visible" : "Collapse";
+    public string Amount => string.Format("{0:0.00 €}", GetBalance(User, Tricount));
+    public string NegativeVisibility => GetBalance(User, Tricount) < 0 ? "Visible" : "Collapsed";
+    public string PositiveVisibility => GetBalance(User, Tricount) >= 0 ? "Visible" : "Collapsed";
+    public string isSelfUserVisibility => User == CurrentUser ? "Visible" : "Collapsed";
+    public string isNotSelfUserVisibility => User != CurrentUser ? "Visible" : "Collapsed";
+    public int BorderWidth => (int) GetAmountBgWidth(Tricount, GetBalance(User, Tricount));
 
 
 
     public UserBalanceCardViewModel(User user, Tricount tricount) : base() {
         _user = user;
         _tricount = tricount;
+    }
+
+    public static Double GetAmountBgWidth(Tricount t, Double curentValue) {
+        Double maxValue = t.CalculateBalances().MaxBy(k => k.Value).Value;
+        Double division = (curentValue / maxValue) * 200;
+        if (division < 0) {
+            return -division;
+        } else {
+            return division;
+        }
+
+    }
+
+    public static Double GetBalance(User u, Tricount t) {
+        return Math.Round(t.CalculateBalances().GetValueOrDefault(u), 2);
     }
 }
